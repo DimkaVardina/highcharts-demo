5b451f292a2a07d53acd9de843f62d9a07bf8a90
Dimka Vardina <dimka.vardina@fourth.com>
2019-08-30 16:36:03 +0300

    First commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Highcharts documentation: https://www.highcharts.com/blog/post/highcharts-wrapper-for-react-101/
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Headline from './components/Headline/Headline';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+import data from './assets/data.js';
+
+
+const options = {
+  chart: {
+    type: 'column',
+    plotBackgroundColor: '#e4e9ed',
+  },
+  time: {
+    timezone: 'Europe/London'
+  },
+  plotOptions: {
+    series: {
+      groupPadding: 0,
+      pointPadding: 0.03,
+      borderWidth: 0
+    }
+  },
+  xAxis: [{
+    grid: { // default setting
+      enabled: true 
+    },
+    gridLineWidth: 1,
+    gridLineColor: '#cbcbcb',
+    scrollbar: {
+        enabled: true
+    },
+    type: 'datetime',
+    dateTimeLabelFormats: {
+        day: '%H:%M'  // https://api.highcharts.com/highstock/xAxis.dateTimeLabelFormats
+    }
+  }],
+  yAxis: [{
+    alternateGridColor: '#ececec',
+    grid: { // default setting
+      enabled: true 
+    },
+    gridLineWidth: 1,
+    gridLineColor: '#cbcbcb',
+  }],
+  series: [{
+    data: data,
+    color: '#80b4cc',
+    pointStart: Date.UTC(2010, 0, 1),
+    pointInterval: 900 * 500  // one day
+  }]
+}
 
 function App() {
+  const greating = 'Hello Highcharts!';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headline value={greating} />
+
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
     </div>
   );
 }
